92b641936b964508498931567297590a1c5c71a7
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-06-25 22:04:06 +0300

    Fix configuration path and clean up app initialization in create_app function

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -9,14 +9,25 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    app.config.from_object('config.Config')
+    app.config.from_object('app.config.Config')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
 
     with app.app_context():
         from . import models
         from .routes import appointments, departments, doctors, patients, medical_records
+
+        # Register blueprints
+        app.register_blueprint(doctors.doctor_bp)
+        app.register_blueprint(patients.patient_bp)
+        app.register_blueprint(appointments.appointment_bp)
+        #app.register_blueprint(departments.department_bp)
+        #app.register_blueprint(medical_records.record_bp)
+
         db.create_all()
 
     return app
